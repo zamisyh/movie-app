ebc4f2b9c97bc706bf14e829f23dca27c6824983
zam-dev <zamsyh.work@gmail.com>
2022-05-19 14:34:55 +0700

    Create redux for add movie

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,18 +2,22 @@ import React, { useEffect } from 'react'
 import MovieListing from '../MovieListing/MovieListing'
 import movieApi from '../../common/apis/movieApi'
 import { APIKey } from '../../common/apis/MovieApiKeys'
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 
 const Home = () => {
+  const movieText = "Harry"
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchMovie = async () => {
-      const movieText = "Harry"
       const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log(err)
         })
 
-      console.log(response)
+        dispatch(addMovies(response.data))
+      
     }
 
     fetchMovie()
