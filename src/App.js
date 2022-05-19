b49f34eaa7327d5cec3b1dcd99400e9071950c4d
zam-dev <zamsyh.work@gmail.com>
2022-05-19 13:34:48 +0700

    Create routing components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react'
 import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home/Home'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/movie/:id' element={<MovieDetail/>}></Route>
+          <Route path="*" element={<PageNotFound/>}></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
