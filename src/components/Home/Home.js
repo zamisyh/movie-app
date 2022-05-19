4e733f94bc605ab1137b57be338122dc3f85cfe7
zam-dev <zamsyh.work@gmail.com>
2022-05-19 14:12:11 +0700

    Use axios for api call

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import MovieListing from '../MovieListing/MovieListing'
+import movieApi from '../../common/apis/movieApi'
+import { APIKey } from '../../common/apis/MovieApiKeys'
 
 const Home = () => {
+
+  useEffect(() => {
+    const fetchMovie = async () => {
+      const movieText = "Harry"
+      const response = await movieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
+        .catch((err) => {
+          console.log(err)
+        })
+
+      console.log(response)
+    }
+
+    fetchMovie()
+  }, [])
+
   return (
-    <div>Home</div>
+    <div>
+      <div className="banner-img"></div>
+      <MovieListing />
+    </div>
   )
 }
 
