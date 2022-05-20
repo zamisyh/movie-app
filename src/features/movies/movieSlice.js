d17bc78c5f1bc8dbcdc0a8a307d508b4c50a6f33
zam-dev <zamsyh.work@gmail.com>
2022-05-20 15:51:33 +0700

    Create shows series

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -14,8 +14,21 @@ export const fetchAsyncMovies = createAsyncThunk(
 );
 
 
+export const fetchAsyncShows = createAsyncThunk(
+    "movies/fetchAsyncShows",
+    async () => {
+      const seriesText = "Friends";
+      const response = await movieApi.get(
+        `?apiKey=${APIKey}&s=${seriesText}&type=series`
+      );
+      return response.data;
+    }
+);
+
+
 const initialState = {
-    movies: []
+    movies: {},
+    shows: {}
 }
 
 const movieSlice = createSlice({
