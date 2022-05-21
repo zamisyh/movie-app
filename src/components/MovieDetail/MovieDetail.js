b2de81c1b0367e63d552667f5f2d3cfe237fc44c
zam-dev <zamsyh.work@gmail.com>
2022-05-21 21:11:32 +0700

    Styling detail movie page

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router'
 import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from '../../features/movies/movieSlice'
+import "./MovieDetail.scss"
 
 const MovieDetail = () => {
 
