f42ef1c9aaf64f52a3f11b5b64507d24ad1c0afb
zam-dev <zamsyh.work@gmail.com>
2022-05-21 20:54:42 +0700

    Create movie detail page

--- src/components/MovieDetail/MovieDetail.js
+++ src/components/MovieDetail/MovieDetail.js
@@ -17,8 +17,52 @@ const MovieDetail = () => {
 
 
   return (
-    <div>
-      <h1>Haloo</h1>
+    <div className="movie-section">
+      <div className="section-left">
+        <div className="movie-title">
+          {data.Title}
+        </div>
+        <div className="movie-rating">
+          <span>
+            Rating <i className="fa fa-star"></i> : {data.imdbRating}
+          </span>
+          <span>
+            Votes <i className="fa fa-thumbs-up"></i> : {data.imdbVotes}
+          </span>
+          <span>
+            Runtime <i className="fa fa-film"></i> : {data.Runtime}
+          </span>
+          <span>
+            Year <i className="fa fa-calendar"></i> : {data.Year}
+          </span>
+        </div>
+        <div className="movie-plot">{data.Plot}</div>
+        <div className="movie-info">
+          <div>
+            <span>Director</span>
+            <span>{data.Director}</span>
+          </div>
+          <div>
+            <span>Stars</span>
+            <span>{data.Actors}</span>
+          </div>
+          <div>
+            <span>Genre</span>
+            <span>{data.Genre}</span>
+          </div>
+          <div>
+            <span>Languages</span>
+            <span>{data.Language}</span>
+          </div>
+          <div>
+            <span>Awards</span>
+            <span>{data.Awards}</span>
+          </div>
+        </div>
+      </div>
+      <div className="section-right">
+        <img src={data.Poster} alt={data.Title} />
+      </div>
     </div>
   )
 }
